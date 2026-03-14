e92a0a97ceedb16457324833df337271a54a3126
Undrakh Orgil Odonkhuu <uodonkhuu@miu.edu>
2026-03-14 16:27:52 -0500

    VCN

---
+++ infra-live/vcn/main.tf
@@ -0,0 +1,17 @@
+module "my_vcn01" {
+    source            = "../../infra-modules/vcn_module"
+    
+    compartment_ocid  = var.compartment_ocid
+    cidr_block        = var.vcn01_cidr_block
+    dns_label         = var.vcn01_dns_label
+    display_name      = var.vcn01_display_name  
+}
+
+module "my_vcn02" {
+    source            = "../../infra-modules/vcn_module"
+
+    compartment_ocid  = var.compartment_ocid
+    cidr_block        = var.vcn02_cidr_block
+    dns_label         = var.vcn02_dns_label
+    display_name      = var.vcn02_display_name  
+}
