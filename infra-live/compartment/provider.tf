af7f1897b2adbbe2c766f5ed38f8d016de3753d1
Undrakh Orgil Odonkhuu <uodonkhuu@miu.edu>
2026-03-14 19:26:20 -0500

    VCN

--- infra-live/compartment/provider.tf
+++ infra-live/compartment/provider.tf
@@ -1,9 +1,12 @@
 terraform {
-    required_providers {
-        oci = {
-            source  = "oracle/oci" 
-        }
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">= 4.0.0"
     }
+  }
 }
 # Use existing OCI CLI config: credentials from ~/.oci/config profile [DEFAULT]
 # (tenancy_ocid, user_ocid, fingerprint, key_file). No need to pass them in Terraform.
